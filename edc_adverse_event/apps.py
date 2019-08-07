6143847c5a659c73fa0747fb3ea52c2ae428f8df
erikvw <ew2789@gmail.com>
2019-08-06 23:16:35 -0500

    exportable

--- edc_adverse_event/apps.py
+++ edc_adverse_event/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig as DjangoAppConfig
 class AppConfig(DjangoAppConfig):
     name = "edc_adverse_event"
     verbose_name = "Edc Adverse Event"
+    has_exportable_data = True
     include_in_administration_section = True
