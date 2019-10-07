084fcdc042ef40ce4eaf7533cd409c81894cdeeb
erikvw <ew2789@gmail.com>
2019-10-07 10:09:50 -0500

    auto close if submitted_datetime

--- edc_adverse_event/model_mixins/ae_susar/ae_susar_methods_model_mixin.py
+++ edc_adverse_event/model_mixins/ae_susar/ae_susar_methods_model_mixin.py
@@ -1,4 +1,5 @@
 from django.db import models
+from edc_constants.constants import CLOSED, OPEN
 
 
 class AeSusarMethodsModelMixin(models.Model):
@@ -7,6 +8,12 @@ class AeSusarMethodsModelMixin(models.Model):
 
     def save(self, *args, **kwargs):
         self.subject_identifier = self.ae_initial.subject_identifier
+        if self.submitted_datetime:
+            self.report_status = CLOSED
+            self.report_closed_datetime = self.submitted_datetime
+        else:
+            self.report_status = OPEN
+            self.report_closed_datetime = None
         super().save(*args, **kwargs)
 
     def natural_key(self):
