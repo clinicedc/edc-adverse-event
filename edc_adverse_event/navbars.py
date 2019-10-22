23d51d96a4d3137041cab0feb36bcf7511414650
erikvw <ew2789@gmail.com>
2019-10-22 01:35:54 -0500

    rename templates, navbars, dashboard_templates, rename home views

--- edc_adverse_event/navbars.py
+++ edc_adverse_event/navbars.py
@@ -4,17 +4,26 @@ from edc_navbar import NavbarItem, site_navbars, Navbar
 
 no_url_namespace = True if settings.APP_NAME == "edc_adverse_event" else False
 
-navbar = Navbar(name="edc_adverse_event")
-
-navbar.append_item(
-    NavbarItem(
-        name="ae",
-        title="Adverse Events",
-        fa_icon="fa-heartbeat",
-        url_name=f"{settings.ADVERSE_EVENT_APP_LABEL}:home_url",
-        codename="edc_navbar.nav_edc_adverse_event",
-        no_url_namespace=no_url_namespace,
-    )
+ae_navbar_item = NavbarItem(
+    name="ae_home",
+    title="Adverse Events",
+    label="AE",
+    codename="edc_navbar.nav_edc_adverse_event",
+    url_name=f"edc_adverse_event:ae_home_url",
+    no_url_namespace=no_url_namespace,
 )
 
-site_navbars.register(navbar)
+tmg_navbar_item = NavbarItem(
+    name="tmg_home",
+    label="TMG",
+    fa_icon="fas fa-chalkboard-teacher",
+    codename="edc_navbar.nav_tmg_section",
+    url_name="edc_adverse_event:tmg_home_url",
+    no_url_namespace=no_url_namespace,
+)
+
+ae_navbar = Navbar(name="edc_adverse_event")
+ae_navbar.append_item(ae_navbar_item)
+ae_navbar.append_item(tmg_navbar_item)
+
+site_navbars.register(ae_navbar)
