6aaac1463f5ffdf7e9c5f4cc495bf044cab7e5cc
erikvw <ew2789@gmail.com>
2020-03-17 16:53:18 -0500

    get subject_identifier_prefix from edc_protocol

--- edc_adverse_event/urls.py
+++ edc_adverse_event/urls.py
@@ -1,7 +1,7 @@
-from django.apps import apps as django_apps
 from django.urls.conf import path
 from django.views.generic.base import RedirectView
 from edc_adverse_event.admin_site import edc_adverse_event_admin
+from edc_protocol.protocol import Protocol
 
 from .views import (
     AeHomeView,
@@ -15,35 +15,31 @@ from .views import (
 
 app_name = "edc_adverse_event"
 
-subject_identifier_pattern = django_apps.get_app_config(
-    "edc_identifier"
-).subject_identifier_pattern  # "092\-[0-9\-]+"
-
 
 urlpatterns = NewTmgAeListboardView.urls(
     namespace=app_name,
     label="new_tmg_ae_listboard",
-    identifier_pattern=subject_identifier_pattern,
+    identifier_pattern=Protocol().subject_identifier_pattern,
 )
 urlpatterns += OpenTmgAeListboardView.urls(
     namespace=app_name,
     label="open_tmg_ae_listboard",
-    identifier_pattern=subject_identifier_pattern,
+    identifier_pattern=Protocol().subject_identifier_pattern,
 )
 urlpatterns += ClosedTmgAeListboardView.urls(
     namespace=app_name,
     label="closed_tmg_ae_listboard",
-    identifier_pattern=subject_identifier_pattern,
+    identifier_pattern=Protocol().subject_identifier_pattern,
 )
 urlpatterns += TmgDeathListboardView.urls(
     namespace=app_name,
     label="tmg_death_listboard",
-    identifier_pattern=subject_identifier_pattern,
+    identifier_pattern=Protocol().subject_identifier_pattern,
 )
 urlpatterns += TmgSummaryListboardView.urls(
     namespace=app_name,
     label="tmg_summary_listboard",
-    identifier_pattern=subject_identifier_pattern,
+    identifier_pattern=Protocol().subject_identifier_pattern,
 )
 urlpatterns += [
     path("tmg/", TmgHomeView.as_view(), name="tmg_home_url"),
