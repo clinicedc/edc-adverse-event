ebc7d071ca421ab0372592b35d2b8615c4142e2f
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2022-03-15 15:56:21 +0000

    Update clean/validation checks to match form admin order

--- edc_adverse_event/form_validators/ae_initial.py
+++ edc_adverse_event/form_validators/ae_initial.py
@@ -5,9 +5,9 @@ from edc_form_validators import FormValidator
 class AeInitialFormValidator(FormValidator):
     def clean(self):
 
-        self.validate_relationship_to_study_drug()
-
         self.validate_other_specify(field="ae_classification")
+
+        self.validate_relationship_to_study_drug()
 
         self.required_if(YES, field="ae_cause", field_required="ae_cause_other")
 
